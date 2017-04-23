91fcad6623be9b0c4cc2ecd4a7f98797d8bdb09d
scott-linder <scott.b.linder@wmich.edu>
2017-04-23 12:06:10 -0400

    Update bench.sh to include benchmark name

--- Testfiles/bench.sh
+++ Testfiles/bench.sh
@@ -6,6 +6,7 @@ for t in arrayparam bubble check dynamic fib newdyn while while_array
 do
   cd $t
   rm -f $t.optA.il $t.optB.il *.out
+  echo $t
   { time -p java -cp $CP codegen.CodeGenerator -A $t.il >$t.optA.il ; } 2>&1 | awk '/real/ { print $2 }'
   { time -p java -cp $CP codegen.CodeGenerator -B $t.il >$t.optB.il ; } 2>&1 | awk '/real/ { print $2 }'
   java -jar ../iloc.jar -s $t.il >$t.out
@@ -13,4 +14,4 @@ do
   java -jar ../iloc.jar -s $t.optB.il >$t.optB.out
   diff3 $t.out $t.optA.out $t.optB.out | sed -n 's/.*= \(.*\)/\1/p'
   cd ..
-done | paste - - - - - -d,
+done | paste - - - - - - -d ,
